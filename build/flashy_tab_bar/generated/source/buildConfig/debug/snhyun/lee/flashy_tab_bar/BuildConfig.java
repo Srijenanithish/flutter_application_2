cd07647750e3be9a0bd4cf84a9853f8836b31e88
Srijenanithish <srijenanithish@gmail.com>
2021-10-01 09:39:49 +0530

    First commit

---
+++ build/flashy_tab_bar/generated/source/buildConfig/debug/snhyun/lee/flashy_tab_bar/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package snhyun.lee.flashy_tab_bar;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "snhyun.lee.flashy_tab_bar";
+  public static final String BUILD_TYPE = "debug";
+}
---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter_application_2/pages/cart.dart';
+import 'package:flutter_application_2/pages/explore.dart';
+import 'package:flashy_tab_bar/flashy_tab_bar.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late PageController _pageController;
+  int _selectedPage = 0;
+
+  List<Widget> pages = [ExplorePage(), CartPage()];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedPage = index;
+      _pageController.jumpToPage(index);
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _pageController = PageController(initialPage: 0);
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        onPageChanged: (index) => setState(() {
+          _selectedPage = index;
+        }),
+        controller: _pageController,
+        children: [...pages],
+      ),
+      bottomNavigationBar: FlashyTabBar(
+        selectedIndex: _selectedPage,
+        showElevation: false,
+        onItemSelected: (index) => _onItemTapped(index),
+        items: [
+          FlashyTabBarItem(
+            icon: Icon(Icons.home_outlined, size: 23),
+            title: Text('Home'),
+          ),
+          FlashyTabBarItem(
+            icon: Icon(Icons.shopping_bag_outlined, size: 23),
+            title: Text('Cart'),
+          ),
+        ],
+      ),
+    );
+  }
+}
